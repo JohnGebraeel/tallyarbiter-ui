7f140bad0f81362a6446ff0ba4a077ae91b9f10c
Guido Visser <guido.vsr@gmail.com>
2020-10-25 00:19:38 +0200

    Initial commit

---
+++ craco.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    //Still need to implement antd theming...
+    /* rules: [
+        {
+            test: /\.less$/,
+            use: [
+                {
+                    loader: "style-loader",
+                },
+                {
+                    loader: "css-loader", // translates CSS into CommonJS
+                },
+                {
+                    loader: "less-loader", // compiles Less to CSS
+                    options: {
+                        lessOptions: {
+                            // If you are using less-loader@5 please spread the lessOptions to options directly
+                            modifyVars: {
+                                "primary-color": "#F00",
+                                "link-color": "#1DA57A",
+                                "border-radius-base": "2px",
+                            },
+                            javascriptEnabled: true,
+                        },
+                    },
+                },
+            ],
+            // ...other rules
+        },
+    ],
+    // ...other config */
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
+import React, { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
+import "antd/dist/antd.css";
+import "./App.scss";
+
+const Header = Layout.Header;
+const Content = Layout.Content;
+const Footer = Layout.Footer;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [response, setResponse] = useState("");
+
+    useEffect(() => {
+        const socket = socketIOClient("http://127.0.0.1:4455");
+        console.log("IK KOM HIER");
+        socket.on("initialdata", function (
+            sourceTypes: any,
+            sourceTypesDataFields: any,
+            outputTypes: any,
+            outputTypesDataFields: any,
+            busOptions: any,
+            sourcesData: any,
+            devicesData: any,
+            deviceSources: any,
+            deviceActions: any,
+            deviceStates: any,
+            tslClients: any,
+            cloudDestinations: any,
+            cloudKeys: any,
+            cloudClients: any
+        ) {
+            console.log(sourceTypes);
+            console.log(sourceTypesDataFields);
+            console.log(outputTypes);
+            console.log(outputTypesDataFields);
+            console.log(busOptions);
+            console.log(sourcesData);
+            console.log(devicesData);
+            console.log(deviceSources);
+            console.log(deviceActions);
+            console.log(deviceStates);
+            console.log(tslClients);
+            console.log(cloudDestinations);
+            console.log(cloudKeys);
+            console.log(cloudClients);
+        });
+        socket.emit("sources");
+        socket.on("sources", (data: any) => console.log(data));
+    }, []);
+
+    return (
+        <div className="App">
+            <Layout>
+                <Header>
+                    <img src="/logo_white.png" height={50} />
+                </Header>
+                <Content>Content</Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
